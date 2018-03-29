8c63a6ea4aed7cb12bb0bd09a5781d243981ff1a
ggn <devnull@localhost>
2018-03-29 09:40:41 +0300

    Okay, let's try this then

--- bigbrownbuild.sh
+++ bigbrownbuild.sh
@@ -40,8 +40,8 @@ mainbrown()
     # so your mileage may vary! Also you might be able to build all gcc versions using one
     # compiler - so many problems were encountered in Ubuntu (including Internal Compiler
     # Errors) that this is now in full pendantic mode. Again, your mileage may vary!
-    CC4=gcc-4.8
-    CXX4=g++-4.8
+    CC4=gcc-5
+    CXX4=g++-5
     CC5=gcc-5
     CXX5=g++-5
     CC6=gcc-6
